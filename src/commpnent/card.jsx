ba9a220d8004aca7b54a1108419b7751dea49316
Ubada <bhassan.ghamsh2020@gmail.com>
2024-07-07 17:55:46 +0300

    new idea

--- src/commpnent/card.jsx
+++ src/commpnent/card.jsx
@@ -3,22 +3,19 @@ import icon from '../images/icon-dice.svg'
 
 const Card = ({data}) => {
     const [count, setCount] = useState("001")
-    const [number, setNumber] = useState(2)
     const numAdvice = () => {
-        if (data.length > number -1) {
-            const n = number + 1
-            const num = number.toString().padStart(3, "0");
-            setNumber(n)
+        if (data.length > +count ) {
+            const n = +count + 1
+            const num = n.toString().padStart(3, "0");
             setCount(num)
         } else {
-            setNumber(2)
             setCount("001")
         }
     }
     return (
         <div className="card">
             <div className="advice">A D V I C E <span> # {count}</span></div>
-            <div className="quote">"{data[number - 2]}"</div>
+            <div className="quote">"{data[+count - 1]}"</div>
             <div className="image"></div>
             <div className="button" onClick={numAdvice}><img src={icon}/></div>
         </div>
